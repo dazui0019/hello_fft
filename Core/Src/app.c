07bfea28b52945028e5ba901dc04586a2b35b94a
dazui0019 <dazui0019@gmail.com>
2025-06-24 17:36:03 +0800

    feat(i2s): add I2S audio input support for FFT analysis
    - Enable I2S module and configure DMA for audio data reception
    - Add I2S initialization and interrupt handlers
    - Modify app.c to process I2S audio data for FFT analysis
    - Update project configuration files to include I2S support

--- Core/Src/app.c
+++ Core/Src/app.c
@@ -1,78 +1,146 @@
 #include "main.h"   // IWYU pragma: keep
-#include "adc.h"
+#include "stm32f4xx_hal_i2s.h"
 #include "tim.h"
 #include "arm_math.h"
 #include "printf.h" // IWYU pragma: keep
 #include "ws28xx.h"
+#include "i2s.h" // IWYU pragma: keep
+#include <stdint.h>
 
 #define FFT_SIZE 1024
 
 void vol2led(float32_t vol);
 
-uint16_t ADC_Value[FFT_SIZE];
-__IO uint8_t FFT_Flag = 0;
+// i2s传输完成标志
+__IO uint8_t i2s_cplt_flag = 0;
+__IO uint8_t i2s_half_flag = 0;
 
 float32_t fft_input_buffer[FFT_SIZE * 2];  // 实部和虚部交错存储
 float32_t fft_output_buffer[FFT_SIZE/2];   // FFT幅度输出(只需要前一半)
 arm_cfft_radix4_instance_f32 fft_instance; // FFT实例
-float32_t sampling_frequency = 10000.0f;   // 采样频率，需要根据您的定时器配置调整
+float32_t sampling_frequency = 48000.0f;   // 采样频率，需要根据您的定时器配置调整
 
 WS28XX_HandleTypeDef hLed;
 
-uint32_t DMA_Buffer[8] = {50, 50, 50, 50, 50, 50, 50, 50};
+uint16_t I2S_Buffer[2*4*FFT_SIZE];    // 一个周期的数据需要4个元素来存储, 包含左右声道. 然后使用了一个乒乓缓冲区
+
+int32_t val_24bit = 0;
 
 void setup(void)
 {
     // 初始化FFT实例
     arm_cfft_radix4_init_f32(&fft_instance, FFT_SIZE, 0, 1);
 
-    __HAL_DMA_CLEAR_FLAG(hadc1.DMA_Handle, DMA_FLAG_TCIF0_4|DMA_FLAG_HTIF0_4);
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_Value, FFT_SIZE);
-    HAL_TIM_Base_Start(&htim2);
-    printf("Hello, FFT!\r\n");
+    // __HAL_DMA_CLEAR_FLAG(hadc1.DMA_Handle, DMA_FLAG_TCIF0_4|DMA_FLAG_HTIF0_4);
+    // HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_Value, FFT_SIZE);
+    // HAL_TIM_Base_Start(&htim2);
+    // printf("Hello, FFT!\r\n");
     WS28XX_Init(&hLed, &htim3, 90, TIM_CHANNEL_2, 64);
+    // HAL_I2S_Receive(&hi2s1, I2S_Buffer, FFT_SIZE, 100);
+    // HAL_I2S_Receive_DMA(&hi2s1, I2S_Buffer, 2*2*FFT_SIZE);
+    HAL_I2S_Receive_DMA(&hi2s1, I2S_Buffer, 4);
 }
 
 void loop(void)
 {
-    WS28XX_Update(&hLed);
-    if(FFT_Flag == 0){
+    // if(FFT_Flag == 1){
+    //     printf("%d\n", I2S_Buffer[2]);
+    //     FFT_Flag = 0;
+    // }
+
+    HAL_Delay(100);
+    return;
+
+    if((i2s_half_flag|i2s_cplt_flag) == 0){
         return;
     }
 
-    FFT_Flag = 1;
-
-    // 将ADC数据转换为复数格式（实部为ADC值，虚部为0）
-    for(int i = 0; i < FFT_SIZE; i++){
-        fft_input_buffer[2*i] = (float32_t)ADC_Value[i];     // 实部
-        fft_input_buffer[2*i + 1] = 0.0f;                    // 虚部
+    if(i2s_half_flag == 1){
+        uint16_t* raw_buff = I2S_Buffer;
+        i2s_half_flag = 0;
+        for(int i = 0; i < FFT_SIZE; i++){
+            val_24bit = ((raw_buff[(4*i)]<<8)|(raw_buff[(4*i)+1]>>8));
+            //将24位有符号整型扩展到32位
+            if(val_24bit & 0x800000){//negative
+                val_24bit |= 0xff000000;
+            }
+            fft_input_buffer[2*i] = (float32_t)val_24bit;   // 实部
+            fft_input_buffer[2*i + 1] = 0.0f;                   // 虚部
+            // printf("[%d]:%d ", i, val_24bit);
+        }
+    }else if(i2s_cplt_flag == 1){
+        uint16_t* raw_buff = &I2S_Buffer[4*FFT_SIZE];
+        i2s_cplt_flag = 0;
+        for(int i = 0; i < FFT_SIZE; i++){
+            val_24bit = ((raw_buff[(4*i)]<<8)|(raw_buff[(4*i)+1]>>8));
+            //将24位有符号整型扩展到32位
+            if(val_24bit & 0x800000){//negative
+                val_24bit |= 0xff000000;
+            }
+            fft_input_buffer[2*i] = (float32_t)val_24bit;   // 实部
+            fft_input_buffer[2*i + 1] = 0.0f;                   // 虚部
+            printf("[%d]:%d ", i, val_24bit);
+        }
     }
 
-    // 执行FFT变换
-    arm_cfft_radix4_f32(&fft_instance, fft_input_buffer);
+    printf("\r\n\r\n");
 
-    // 只计算小于奈奎斯特频率的幅度谱
-    arm_cmplx_mag_f32(fft_input_buffer, fft_output_buffer, FFT_SIZE/2);
+    // arm_cfft_radix4_f32(&fft_instance, fft_input_buffer);
 
-    // 计算1K-1.5KHz的FFT幅度
-    float32_t freq_resolution = sampling_frequency / FFT_SIZE;  // 频率分辨率
-    int start_bin = (int)(1000.0f / freq_resolution);          // 1KHz对应的频率bin
-    int end_bin = (int)(1500.0f / freq_resolution);            // 1.5KHz对应的频率bin
-    
-    // 计算指定频率范围内的平均幅度
-    float32_t total_magnitude = 0.0f;
-    for(int i = start_bin; i <= end_bin && i < FFT_SIZE/2; i++){
-        total_magnitude += fft_output_buffer[i];
-    }
-    float32_t avg_magnitude = total_magnitude / (end_bin - start_bin + 1);
-    
-    // 根据音频幅值控制LED
-    vol2led(avg_magnitude);
+    // // 只计算小于奈奎斯特频率的幅度谱
+    // arm_cmplx_mag_f32(fft_input_buffer, fft_output_buffer, FFT_SIZE/2);
 
-    // 重新启动ADC采集
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_Value, FFT_SIZE);
-    HAL_TIM_Base_Start(&htim2);
+    // // 计算主要频率
+    // int max_bin = 0;
+    // for(int i = 1; i < FFT_SIZE/2; i++){
+    //     if(fft_output_buffer[i] > fft_output_buffer[max_bin]){
+    //         max_bin = i;
+    //     }
+    // }
+    // float32_t freq = max_bin * sampling_frequency / FFT_SIZE;
+    // printf("Freq: %f Hz\n", freq);
 }
+
+// void loop(void)
+// {
+//     WS28XX_Update(&hLed);
+//     if(FFT_Flag == 0){
+//         return;
+//     }
+
+//     FFT_Flag = 1;
+
+//     // 将ADC数据转换为复数格式（实部为ADC值，虚部为0）
+//     for(int i = 0; i < FFT_SIZE; i++){
+//         fft_input_buffer[2*i] = (float32_t)ADC_Value[i];     // 实部
+//         fft_input_buffer[2*i + 1] = 0.0f;                    // 虚部
+//     }
+
+//     // 执行FFT变换
+//     arm_cfft_radix4_f32(&fft_instance, fft_input_buffer);
+
+//     // 只计算小于奈奎斯特频率的幅度谱
+//     arm_cmplx_mag_f32(fft_input_buffer, fft_output_buffer, FFT_SIZE/2);
+
+//     // 计算1K-1.5KHz的FFT幅度
+//     float32_t freq_resolution = sampling_frequency / FFT_SIZE;  // 频率分辨率
+//     int start_bin = (int)(1000.0f / freq_resolution);          // 1KHz对应的频率bin
+//     int end_bin = (int)(1500.0f / freq_resolution);            // 1.5KHz对应的频率bin
+    
+//     // 计算指定频率范围内的平均幅度
+//     float32_t total_magnitude = 0.0f;
+//     for(int i = start_bin; i <= end_bin && i < FFT_SIZE/2; i++){
+//         total_magnitude += fft_output_buffer[i];
+//     }
+//     float32_t avg_magnitude = total_magnitude / (end_bin - start_bin + 1);
+    
+//     // 根据音频幅值控制LED
+//     vol2led(avg_magnitude);
+
+//     // 重新启动ADC采集
+//     HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_Value, FFT_SIZE);
+//     HAL_TIM_Base_Start(&htim2);
+// }
 
 void vol2led(float32_t vol)
 {
@@ -121,10 +189,14 @@ void vol2led(float32_t vol)
     }
 }
 
-// dma 中断回调函数
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+void HAL_I2S_RxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
 {
-    HAL_TIM_Base_Stop(&htim2);
-    HAL_ADC_Stop_DMA(hadc);
-    FFT_Flag = 1;
+    // 半满
+    i2s_half_flag = 1;
+}
+
+void HAL_I2S_RxCpltCallback(I2S_HandleTypeDef *hi2s)
+{
+    // 全满
+    i2s_cplt_flag = 1;
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "adc.h"
 #include "dma.h"
+#include "i2s.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -95,6 +96,7 @@ int main(void)
   MX_TIM2_Init();
   MX_USART2_UART_Init();
   MX_TIM3_Init();
+  MX_I2S1_Init();
   /* USER CODE BEGIN 2 */
   setup();
   /* USER CODE END 2 */
